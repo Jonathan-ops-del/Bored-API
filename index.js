4a99ba98501e3e8b380712673a1af3294e79c4fd
Jonathan.Francis <jjonathan.francis@gmail.com>
2023-10-24 22:48:05 -0400

    update Procfile

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -43,6 +42,11 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started Successfully");
 });
